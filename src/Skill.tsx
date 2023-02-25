be1d867d1a5fb22fda57d71d69dd1b80123be2ae
Guilherme Figueira <guibf2004@gmail.com>
2023-02-25 14:45:51 -0300

    caminho icone

--- src/Skill.tsx
+++ src/Skill.tsx
@@ -8,7 +8,7 @@ export default function Skill({ category, score, icon }: SkillProps) {
 	return (
 		<div>
 			<div>
-				<img src={`${icon}`} alt={`${category} icon`} />
+				<img src={icon} alt={`${category} icon`} />
 			</div>
 			<div>{category}</div>
 			<div>{score} /100</div>
