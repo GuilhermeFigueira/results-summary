d30e5a851bbbe7f9d98954963858b16c52726c2c
Guilherme Figueira <guibf2004@gmail.com>
2023-02-25 14:32:29 -0300

    configurações finalizadas inicio do mapeamento json

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
+import TestResults from "./data.json";
+
 function App() {
 	return (
-		<div>
-			<div className="text-center">
-				Challenge by{" "}
-				<a
-					href="https://www.frontendmentor.io?ref=challenge"
-					target="_blank"
-				>
-					Frontend Mentor
-				</a>
-				. Coded by{" "}
-				<a
-					href="https://github.com/guilhermefigueira"
-					target="_blank"
-					className="text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-sky-700 font-bold hover:text-lg transition-all"
-				>
-					Guilherme Figueira
-				</a>
-				.
+		<>
+			<main>
+				<h1>Your Result</h1>
+				<div>
+					<h2>76</h2>
+					<span>of 100</span>
+				</div>
+				<h3>Great</h3>
+				<footer>
+					You scored higher than 65% of the people who have taken
+					these tests.
+				</footer>
+			</main>
+			<aside>
+				<title>Summary</title>
+				{TestResults.map((results) => {
+					return (
+						<Skill
+							key={results.category}
+							category={results.category}
+							icon={results.icon}
+							score={results.score}
+						/>
+					);
+				})}
+
+				<button>Continue</button>
+			</aside>
+			<div>
+				<div className="text-center ">
+					Challenge by{" "}
+					<a
+						href="https://www.frontendmentor.io?ref=challenge"
+						target="_blank"
+					>
+						Frontend Mentor
+					</a>
+					. Coded by{" "}
+					<a
+						href="https://github.com/guilhermefigueira"
+						target="_blank"
+						className="text-transparent bg-clip-text bg-gradient-to-r from-rose to-sky font-bold hover:text-lg transition-all "
+					>
+						Guilherme Figueira
+					</a>
+					.
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.tsx", "./index.html"],
+	theme: {
+		fontFamily: { sans: ["Hanken_Grotesk", "sans-serif"] },
+		extend: {
+			colors: {
+				white: "hsl(0, 0%, 100%)",
+				sky: "#0369a1",
+				rose: "#be123c",
+				red: "hsl(0, 100%, 67%)",
+				yellow: "hsl(39, 100%, 56%)",
+				green: "hsl(166, 100%, 37%)",
+				blue: {
+					cobalt: "hsl(234, 85%, 45%)",
+					slate: "hsl(252, 100%, 67%)",
+					royal: "hsl(241, 81%, 54%)",
+					violet: "hsla(256, 72%, 46%, 1)",
+					persian: "hsla(241, 72%, 46%, 0)",
+					pale: "hsl(221, 100%, 96%)",
+					dark: "hsl(224, 30%, 27%)",
+					lavender: "hsl(241, 100%, 89%)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
